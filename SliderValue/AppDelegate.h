5474b48d725fa778adf7658fc5726736e1c452a1
RK905 <mr.rayenk@gmail.com>
2015-07-09 15:06:28 -0400

    Initial Commit

---
+++ SliderValue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderValue
+//
+//  Created by Rayen Kamta on 7/9/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SliderValue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SliderValue
+//
+//  Created by Rayen Kamta on 7/9/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
